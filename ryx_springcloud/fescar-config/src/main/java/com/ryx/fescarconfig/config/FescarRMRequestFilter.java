e24aa9d30d3d57fa0dcb48666c3f08f45f2e48e1
任原新 <renyuanxin001@icloud.com>
2019-09-08 23:52:20 +0800

    修改备注

--- ryx_springcloud/fescar-config/src/main/java/com/ryx/fescarconfig/config/FescarRMRequestFilter.java
+++ ryx_springcloud/fescar-config/src/main/java/com/ryx/fescarconfig/config/FescarRMRequestFilter.java
@@ -1,5 +1,6 @@
 package com.ryx.fescarconfig.config;
 
+
 import com.alibaba.fescar.core.context.RootContext;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -13,6 +14,7 @@ import java.io.IOException;
 
 @Slf4j
 public class FescarRMRequestFilter extends OncePerRequestFilter {
+
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -36,5 +38,4 @@ public class FescarRMRequestFilter extends OncePerRequestFilter {
             }
         }
     }
-
 }
