954aae8364bab8cbdf73a4d6ee599abeb6cabb96
任原新 <renyuanxin001@icloud.com>
2019-09-08 23:30:51 +0800

    修改备注

---
+++ ryx_springcloud/fescar-config/src/main/java/com/ryx/fescarconfig/FescarConfigApplication.java
@@ -0,0 +1,13 @@
+package com.ryx.fescarconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FescarConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FescarConfigApplication.class, args);
+    }
+
+}
