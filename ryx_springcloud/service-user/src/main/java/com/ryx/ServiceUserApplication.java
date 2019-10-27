5f67c7939490d8baafbb2e6f6fb9584f21e74f15
任原新 <renyuanxin001@icloud.com>
2019-10-27 12:37:25 +0800

    修改入口方法

--- ryx_springcloud/service-user/src/main/java/com/ryx/ServiceUserApplication.java
+++ ryx_springcloud/service-user/src/main/java/com/ryx/ServiceUserApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 入口方法
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 @MapperScan("com.ryx.mapper")
